49cfdc87d02929ca96b5ef4f0ee642a0a7e7c45c
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-05-30 06:24:04 +0200

    exercise

--- Scripts/code.js
+++ Scripts/code.js
@@ -1,3 +1,16 @@
 let data = ["Keenan", 4, 6, 9, 11, "Timmie", 22]
 console.log(data)
-let NumberHolder 
+let NumberHolder = data.filter(Number)
+
+console.log(NumberHolder)
+
+console.log(data.find(value => value == 4))
+console.log(data.findIndex(value => value == 4))
+let OnlyNumbers = data.filter(numb => typeof numb === `number`)
+let TotalNumbers = OnlyNumbers.reduce((numb1, numb2) => numb1 + numb2, 0) 
+console.log(TotalNumbers)
+
+//Spread Operator and and Concat
+
+let Slicer = data.slice(1, 4)
+console.log(Slicer)
